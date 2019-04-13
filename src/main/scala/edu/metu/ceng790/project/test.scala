f3b5a602bdc3173f890f6cf9f7ca995673cb3239
Burak Akar <saitburakakar@gmail.com>
2019-04-13 12:21:05 +0300

    Initial Commit

---
+++ src/main/scala/edu/metu/ceng790/project/test.scala
@@ -0,0 +1,20 @@
+package edu.metu.ceng790.project
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+object test {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("Assignment-2_Part-1:Getting Your Own Recommendation")
+      .set("spark.executor.memory", "5g")
+    //Create Spark Context
+    val sc = new SparkContext(conf)
+    //Set log level
+    sc.setLogLevel("ERROR")
+
+    println("Hello World")
+  }
+}
